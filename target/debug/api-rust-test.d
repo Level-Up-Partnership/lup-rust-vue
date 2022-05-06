5435e0a3c545fd70c9d3da98d0a6f39bc425dde9
Jess Nguyen <nguyjess@gmail.com>
2022-05-05 21:25:16 -0400

    fix gitignore

--- target/debug/api-rust-test.d
+++ target/debug/api-rust-test.d
@@ -1,1 +1,1 @@
-C:\Users\Hadi\Documents\github\lup-rust-vue\target\debug\api-rust-test.exe: C:\Users\Hadi\Documents\github\lup-rust-vue\src\main.rs
+C:\Users\JS\Documents\github\rust-rocket-test\rust-app\target\debug\api-rust-test.exe: C:\Users\JS\Documents\github\rust-rocket-test\rust-app\src\main.rs
--- target/debug/deps/api_rust_test.d
+++ target/debug/deps/api_rust_test.d
@@ -1,5 +1,5 @@
-C:\Users\Hadi\Documents\github\lup-rust-vue\target\debug\deps\api_rust_test.exe: src/main.rs
+C:\Users\JS\Documents\github\rust-rocket-test\rust-app\target\debug\deps\api_rust_test.exe: src/main.rs
 
-C:\Users\Hadi\Documents\github\lup-rust-vue\target\debug\deps\api_rust_test.d: src/main.rs
+C:\Users\JS\Documents\github\rust-rocket-test\rust-app\target\debug\deps\api_rust_test.d: src/main.rs
 
 src/main.rs:
