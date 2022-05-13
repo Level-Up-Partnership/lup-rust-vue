cb40878de04daf992e96fbd5f5910eded41e0663
hadifayad9 <hadifayad9@hotmail.com>
2022-05-12 20:25:00 -0400

    changed main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,11 @@
-// #![feature(plugin)]
-// #![plugin(rocket_codegen)]
-
-#![feature(proc_macro_hygiene, decl_macro, plugin)]
-
-#[macro_use]
-extern crate rocket;
-
-extern crate rocket_cors;
-extern crate serde;
-extern crate serde_derive;
-extern crate serde_json;
-
-use rocket::fs::NamedFile;
-use rocket::http::Method;
-use rocket_cors::{AllowedHeaders, AllowedOrigins, Cors, CorsOptions};
-use serde::Serialize;
-
-use std::io;
-use std::path::{Path, PathBuf};
+#[macro_use] extern crate rocket;
 
 #[get("/")]
-async fn index() -> io::Result<NamedFile> {
-    NamedFile::open("webui/dist/index.html").await
-}
-
-#[get("/<file..>")]
-async fn files(file: PathBuf) -> Option<NamedFile> {
-    NamedFile::open(Path::new("webui/dist/").join(file))
-        .await
-        .ok()
-}
-
-#[derive(Serialize, Debug)]
-struct Test {
-    message: &'static str,
-}
-
-#[get("/hello")]
-fn hello() -> String {
-    let hello_message = Test {
-        message: "hello world",
-    };
-    serde_json::to_string(&hello_message).unwrap()
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, files, hello])
+    rocket::build().mount("/", routes![index])
 }
